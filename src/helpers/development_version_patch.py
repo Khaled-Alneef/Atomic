cc1f7e23b3ee03040841cc2463c0a92ff2cb21b9
Khaled Alneef <k.alneef0@gmail.com>
2026-08-29 02:17:38 +0300

    1.10.128 - finalize visual global search

--- src/helpers/development_version_patch.py
+++ src/helpers/development_version_patch.py
@@ -3,7 +3,7 @@
 
 def install():
     from . import updater
-    updater.APP_VERSION = "1.10.127"
+    updater.APP_VERSION = "1.10.128"
     try:
         updater._HEADERS["User-Agent"] = f"Atomic/{updater.APP_VERSION}"
     except Exception:
