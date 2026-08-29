df4e018525cf62756ca1aac59cad7f7ac2f01559
Khaled Alneef <k.alneef0@gmail.com>
2026-08-29 15:15:43 +0300

    1.10.143 - reader player and monitor fixes

--- src/helpers/development_version_patch.py
+++ src/helpers/development_version_patch.py
@@ -3,7 +3,7 @@
 
 def install():
     from . import updater
-    updater.APP_VERSION = "1.10.142"
+    updater.APP_VERSION = "1.10.143"
     try:
         updater._HEADERS["User-Agent"] = f"Atomic/{updater.APP_VERSION}"
     except Exception:
