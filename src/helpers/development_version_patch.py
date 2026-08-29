0bb1500c648f9e9a9dbeca1f1d2865323cb914ba
Khaled Alneef <k.alneef0@gmail.com>
2026-08-29 16:13:25 +0300

    1.10.147 - bump development version

--- src/helpers/development_version_patch.py
+++ src/helpers/development_version_patch.py
@@ -3,7 +3,7 @@
 
 def install():
     from . import updater
-    updater.APP_VERSION = "1.10.146"
+    updater.APP_VERSION = "1.10.147"
     try:
         updater._HEADERS["User-Agent"] = f"Atomic/{updater.APP_VERSION}"
     except Exception:
