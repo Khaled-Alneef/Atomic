99f4c9d4c03cfdef38d9250a31732ff4bd516435
Khaled Alneef <k.alneef0@gmail.com>
2026-08-29 15:31:39 +0300

    1.10.144 - finalize four regression fixes

--- src/helpers/development_version_patch.py
+++ src/helpers/development_version_patch.py
@@ -3,7 +3,7 @@
 
 def install():
     from . import updater
-    updater.APP_VERSION = "1.10.143"
+    updater.APP_VERSION = "1.10.144"
     try:
         updater._HEADERS["User-Agent"] = f"Atomic/{updater.APP_VERSION}"
     except Exception:
