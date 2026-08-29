8a7338392b70fe116e02f9def26ff5559e58fd38
Khaled Alneef <k.alneef0@gmail.com>
2026-08-29 03:18:02 +0300

    1.10.133 - finalize search interaction fixes

--- src/helpers/development_version_patch.py
+++ src/helpers/development_version_patch.py
@@ -3,7 +3,7 @@
 
 def install():
     from . import updater
-    updater.APP_VERSION = "1.10.132"
+    updater.APP_VERSION = "1.10.133"
     try:
         updater._HEADERS["User-Agent"] = f"Atomic/{updater.APP_VERSION}"
     except Exception:
