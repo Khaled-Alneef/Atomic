0c655b99a3817b3e193ad1af9f80124f0e150fca
Khaled Alneef <k.alneef0@gmail.com>
2026-08-29 04:35:27 +0300

    1.10.138 - finalize SVP smooth-motion player

--- src/helpers/development_version_patch.py
+++ src/helpers/development_version_patch.py
@@ -3,7 +3,7 @@
 
 def install():
     from . import updater
-    updater.APP_VERSION = "1.10.137"
+    updater.APP_VERSION = "1.10.138"
     try:
         updater._HEADERS["User-Agent"] = f"Atomic/{updater.APP_VERSION}"
     except Exception:
